982605b06ffa81155e46da83576bda536f406ec5
09NINE90 <razumov.aleksei101@yandex.ru>
2025-11-13 09:45:00 +0500

    Проверка пользователя на вхождение в группу перед стартом в боте

--- app/bot/commands/start.py
+++ app/bot/commands/start.py
@@ -1,15 +1,24 @@
+import logging
+
 from aiogram import types
 
 from app.bot.callbacks.register_user import register_user
 from app.bot.constants.group_id import GROUP_ID
 from app.bot.keyboard_markup import main_markup
+from app.bot.users.is_user_in_chat import is_user_in_chat
+
 
 async def start(message: types.Message):
     if message.chat.id == GROUP_ID:
         return
     user = message.from_user
-    await register_user(user)
 
+    is_valid_user = await is_user_in_chat(user.id, GROUP_ID)
+    if is_valid_user:
+        await register_user(user)
+    else:
+        logging.warn(f"User {user.id} is not in the chat")
+        await message.answer("😔 Вам нельзя пользоваться этим ботом, так как вы не состоите в чате парковки офиса")
     await message.answer(
         "🚗 Бот распределения парковочных мест\n\n"
         "Выберите действие:",
