b8752cb31768b8381c21b508092965cf12b0599c
09NINE90 <razumov.aleksei101@yandex.ru>
2025-12-09 20:49:14 +0500

    Поправлен запрос для статистики запросов на места пользователя

--- app/bot/notification/messages/to_remind_user_of_spot.py
+++ app/bot/notification/messages/to_remind_user_of_spot.py
@@ -7,7 +7,8 @@ async def to_remind_user_of_spot(remind_data: ParkingReminder):
 
     message_text = (
         f"🔔 <b>Напоминание</b>\n"
-        f"На завтра <u>{remind_data.release_date.strftime('%d.%m.%Y')}</u> Вам забронировано место <b>№{remind_data.spot_id}</b>\n\n"
+        f"На завтра <u>{remind_data.release_date.strftime('%d.%m.%Y')}</u> "
+        f"Вам забронировано место <b>№{remind_data.spot_id}</b>\n\n"
         f"ℹ️ <i>Успейте подтвердить место до <u>{cancel_time.strftime('%d.%m %H:%M')}</u> "
         f"иначе оно автоматически уйдет другому человеку</i>"
     )
