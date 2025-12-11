c95f494d21adcf61c71fb61a98be520c0f6c4922
09NINE90 <razumov.aleksei101@yandex.ru>
2025-12-11 17:44:54 +0500

    Дополнительная обработка при редактировании сообщения

--- app/bot/notification/edit_message.py
+++ app/bot/notification/edit_message.py
@@ -1,4 +1,6 @@
-from app.logs.log_builder import log
+from aiogram.exceptions import TelegramBadRequest
+
+from app.logs.log_builder import log, LogType
 
 
 async def edit_message(tg_chat_id, editing_message_id, new_message_text):
@@ -8,6 +10,16 @@ async def edit_message(tg_chat_id, editing_message_id, new_message_text):
             chat_id=tg_chat_id,
             message_id=editing_message_id,
             text=new_message_text
+        )
+    except TelegramBadRequest as e:
+        if "message is not modified" in str(e):
+            await log(
+                log_type=LogType.WARN,
+                log_message=f"Новый текст сообщения похож на старый: {e}"
+            )
+            return
+        await log(
+            log_message=f"TelegramBadRequest при редактировании сообщения: {e}",
         )
     except Exception as e:
         await log(
