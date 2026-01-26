9b611dc50f01e90e354c1f63c748ecffa63ea0de
09NINE90 <razumov.aleksei101@yandex.ru>
2026-01-26 10:28:57 +0500

    Добавлен текс с напоминанием подтвердить место

--- app/bot/notification/messages/to_user_about_assigned_spot.py
+++ app/bot/notification/messages/to_user_about_assigned_spot.py
@@ -1,8 +1,21 @@
+from datetime import datetime, timedelta
+
 from app.bot.utils import get_user_full_mention
 
 
 async def to_user_about_assigned_spot(tg_user_id: int, spot_number: int, assignment_date):
     user = await get_user_full_mention(tg_user_id)
+
+    datetime_now = datetime.now()
+    today_18_00 = datetime_now.replace(hour=18, minute=0, second=0, microsecond=0)
+    tomorrow = datetime_now.date() + timedelta(days=1)
+
+    if (assignment_date == tomorrow) and (datetime_now >= today_18_00):
+        info_text = ""
+    else:
+        info_text = ("\n\n"
+                     "⚠️ <i>Не забудте подвердить место накануне назначенной даты с 18:00 до 00:00</i>")
+
 
     message_text = (
         f"Приветствую, {user}\n\n"
@@ -10,6 +23,7 @@ async def to_user_about_assigned_spot(tg_user_id: int, spot_number: int, assignm
         f"📍 <b>Место:</b> №{spot_number}\n"
         f"📅 <b>Дата:</b> {assignment_date.strftime('%d.%m.%Y')}\n\n"
         f"Поздравляем с получением места!"
+        f"{info_text}"
     )
 
     return message_text
